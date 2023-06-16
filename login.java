6575ac703ff1d200cf7f0aba9161464865deba7a
sadanandRB <sadanandrbirada975@gmail.com>
2023-06-16 10:54:08 +0530

    added for loop

--- login.java
+++ login.java
@@ -1,2 +1,14 @@
-lsdjfjdnvsjfeo
+public class login
+      {
+	      public static void main(strig orgs)
+	      {
+		      fir (i=0;i<=10;i++)
+               {
+		       system .out.println("numbers:+i)
+	       }
+	      }
+      }
+				      
+			      
+			   
 
